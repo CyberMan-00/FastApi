005b507ea22792e19ca70e4a1239d7b14e2f55c3
Miran Shamaev <61889214+CyberMan-00@users.noreply.github.com>
2024-06-12 22:10:18 +0200

    testing commit

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 from core.config import settings
 
 app = FastAPI(
-    title=settings.PROJECT_NAME,
-    version=settings.PROJECT_VERSION
+    # title=settings.PROJECT_NAME,
+    # version=settings.PROJECT_VERSION
 )
 
 
